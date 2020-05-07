58b4a4cd17c922d37373a65a14a54eee61ce0650
dcampos1706 <dvd-campos@hotmail.com>
2020-05-06 23:09:36 -0600

    Se puede abrir cualquier archivo .veisick en la carpeta

--- src/veisick/VEISICK.java
+++ src/veisick/VEISICK.java
@@ -11,37 +11,42 @@ public class VEISICK {
         // TODO code application logic here
 
         try {
-            // Lee el archivo .veisick
-            Scanner leerArchivo = new Scanner(new File(new File("Ecuaciones2doGrado.veisick").getAbsolutePath()));
-            // Crea el analizador léxico.
-            AnalizadorLexico objLexico = new AnalizadorLexico();
-            // Creo el analizador de líneas para el archivo .veisick
-            ValidaLineas objLineas = new ValidaLineas();
-            // Comienzo la lectura del archivo
-            boolean esValido=false;
-            CrearArchivoBas objBas = new CrearArchivoBas();
-            if (objLineas.validaLineas()) {
-                while (leerArchivo.hasNextLine()) {
-                    String line = "";
+            if (args[0].endsWith(".veisick")) {
+                File archivo = new File(args[0]);
 
-                    line = leerArchivo.nextLine(); // Leo la primer línea del archivo
-                    line = line.replaceFirst("\\s{2,}", " ");
-                    line = line.replaceFirst("PRINT\\s{1,}", "PRINT ");
-                    line = line.replaceFirst("INPUT\\s{0,}", "INPUT ");
-                    line = line.replaceFirst(" = ", "=").replaceFirst(" - ", "-");
-                    line = line.replace(" ^ ", "^").replace(" * ", "*").replace(" / ", "/");
-                    line = line.replace(" ( ", "(").replace(" ) ", ")").replace("B + ", "B+").replace(" <= ", "<=");
-                    line = line.replace(", A", ",A").replace(", B", ",B").replace(", C", ",C");
-                    esValido = objLexico.setTexto(line); // Envía el reglón.
-                    if(esValido==false)
-                        break;
+                // Lee el archivo .veisick
+                Scanner leerArchivo = new Scanner(new File(archivo.getAbsolutePath()));
+                // Crea el analizador léxico.
+                AnalizadorLexico objLexico = new AnalizadorLexico();
+                // Creo el analizador de líneas para el archivo .veisick
+                ValidaLineas objLineas = new ValidaLineas();
+                // Comienzo la lectura del archivo
+                boolean esValido = false;
+                CrearArchivoBas objBas = new CrearArchivoBas();
+                if (objLineas.validaLineas()) {
+                    while (leerArchivo.hasNextLine()) {
+                        String line = "";
+
+                        line = leerArchivo.nextLine(); // Leo la primer línea del archivo
+                        line = line.replaceFirst("\\s{2,}", " ");
+                        line = line.replaceFirst("PRINT\\s{1,}", "PRINT ");
+                        line = line.replaceFirst("INPUT\\s{0,}", "INPUT ");
+                        line = line.replaceFirst(" = ", "=").replaceFirst(" - ", "-");
+                        line = line.replace(" ^ ", "^").replace(" * ", "*").replace(" / ", "/");
+                        line = line.replace(" ( ", "(").replace(" ) ", ")").replace("B + ", "B+").replace(" <= ", "<=");
+                        line = line.replace(", A", ",A").replace(", B", ",B").replace(", C", ",C");
+                        esValido = objLexico.setTexto(line); // Envía el reglón.
+                        if (esValido == false) {
+                            break;
+                        }
+                    }
+
                 }
-                
+                if (esValido) {
+                    objBas.abrirArchivo();
+                }
             }
-            if(esValido)
-            {
-                objBas.abrirArchivo();
-            }
+
             //System.out.print("\"([A-Za-z0-9-]+).{1,}\"");
         } catch (FileNotFoundException e) {
             System.out.println("Error: " + e.toString());
